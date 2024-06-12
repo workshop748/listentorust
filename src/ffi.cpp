aa147073e69dac0dedb79a14d66701251b16d9a9
Jacob Peacy <jacobpeacy11@gmail.com>
2024-06-11 22:03:49 -0500

    start on listener

--- src/ffi.cpp
+++ src/ffi.cpp
@@ -1,6 +1,36 @@
 #include <stdint.h>
+#include <sys/epoll.h>
+#include <stdio.h>
+#include <errno.h>
+
+class EventLoop {
+    public:
+    EventLoop() {
+        fd = epoll_create(sizeof(events));
+    }
+    void RegisterPickles(int fd, uint32_t allergies, void* ptr) {
+        epoll_event event;
+        event.data.ptr = ptr;
+        event.events = allergies;
+        int value = epoll_ctl(this->fd, EPOLL_CTL_ADD, fd, &event);
+        fprintf(stderr, "%i, %i\n", value, (int)errno);
+        //EPOLLIN
+    }
+    int fd; // see wallstreetbets for info on what this is (Google WSB fd)
+    epoll_event events[42];
+    void justWaiting() {
+        epoll_wait(fd, events, sizeof(events), -1);
+    }
+};
+
 extern "C" {
-    int32_t MeaningOfLifeTheUniverseAndEverything() {
-        return 42;
+    EventLoop* MeaningOfLifeTheUniverseAndEverything() {
+        return new EventLoop();
+    }
+    void MeaningOfPickes(EventLoop* loop, int fd, uint32_t allergies, void* ptr) {
+        loop->RegisterPickles(fd, allergies, ptr);
+    }
+    void justWaiting(EventLoop* loop) {
+        loop->justWaiting();
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,98 @@
-use std::task::Poll;
+use std::{ffi::c_void, io, num::Wrapping, pin::Pin, sync::Arc, task::{Context, Poll, Wake}};
 
 use futures::Future;
-use tokio::net::TcpStream;
+use std::net::TcpListener;
 
 extern "C" {
-    fn MeaningOfLifeTheUniverseAndEverything() -> i32;
+    fn MeaningOfLifeTheUniverseAndEverything() -> *mut c_void;
+    fn MeaningOfPickes(valA: *mut c_void,valB:i32,valC:u32,valD: *mut c_void);
+        // as I sTARED LONGLYINGLY INTO THE C_VOID I KNEW I COULD NEVER ESC C.
+        fn justWaiting(valA: *mut c_void);
 }
 
+struct NoOpWaker{}
 
-#[tokio::main]
-async fn main() {
-    unsafe {
-        eprintln!("The meaning of life is {}", MeaningOfLifeTheUniverseAndEverything());
+impl Wake for NoOpWaker {
+    fn wake(self: std::sync::Arc<Self>) {
+        
     }
-    let mut tasks = vec![];
-    let mut target = "192.168.86.42";
-    for i in 0..65535 {
-        tasks.push(async move{
-            let connection = TcpStream::connect(format!("{target}:{i}")).await;
-            if connection.is_ok() {
-                eprintln!("Port {i} is open");
+}
+
+struct Yielder {
+    dundy: bool,
+}
+
+impl Yielder {
+    fn new() -> Self {
+        Self{dundy: false}
+    }
+}
+
+impl Future for Yielder {
+    type Output = ();
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        if self.dundy {
+            return Poll::Ready(());
+        }
+        self.dundy = true;
+        Poll::Pending
+    }
+}
+
+async fn async_main() {
+    eprintln!("Hello world!");
+    Yielder::new().await;
+    eprintln!("Hello world 2!");
+}
+
+async fn real_async_main() {
+    let anwser=unsafe{MeaningOfLifeTheUniverseAndEverything()};
+   let brian =anwser;
+    let getSome =unsafe{MeaningOfPickes(anwser, 2, 1, brian)}; 
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let mut fut = std::pin::pin!(async_main());
+    listener.set_nonblocking(true).expect("to go faster press alt f4");//comedy
+    for stream in listener.incoming() {
+        match stream {
+            Ok(s) => {
+                // do something with the TcpStream
+                //handle_connection(s);
             }
-        });
+            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+                // wait until network socket is ready, typically implemented
+                // via platform-specific APIs such as epoll or IOCP
+                //wait_for_fd();
+                // yield here
+                Yielder::new().await;
+                eprintln!("{}", whateveryouwanttoprintgoeshere);
+                continue;
+            }
+            Err(e) => panic!("encountered IO error: {e}"),
+        }
     }
-    futures::future::join_all(tasks).await;
+}
+
+fn main() {
+    unsafe {
+        eprintln!("The meaning of life is {:?}", MeaningOfLifeTheUniverseAndEverything());
+    }
+    let anwser=unsafe{MeaningOfLifeTheUniverseAndEverything()};
+   let brian =anwser;
+    let getSome =unsafe{MeaningOfPickes(anwser, 2, 1, brian)}; 
+    let waker = Arc::new(NoOpWaker{}).into();
+    let mut fut = std::pin::pin!(async_main());
+    let _ = fut.as_mut().poll(&mut Context::from_waker(&waker));
+    eprintln!("Got here");
+    let _ = fut.as_mut().poll(&mut Context::from_waker(&waker));
+
+    let mut fut = std::pin::pin!(real_async_main());
+    loop {
+        if matches!(fut.as_mut().poll(&mut Context::from_waker(&waker)), Poll::Ready(_)) {
+            break;
+        }
+        // call epoll_wait
+        unsafe{justWaiting(brian);}
+
+    }
 }
